73a7dfed1104b138f2ca5854ce8458ad4a4fb469
Martina Bradic <mb3188@gmail.com>
2016-05-26 17:23:21 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,6 +15,8 @@
 #      GFF should be same GFF used to build snpEff db
 
 
+#ADJUST FOR YOUR DIRECTORIES BELOW
+
 PLOIDY=1
 GLOBALREALIGNMENT=0
 FQTABLE=/data/cgsb/carlton/MB_ddRAD/MB_BACKUP/TD_SEQ_PAPAER_ANALYSIS/TD_SEQ_ANALYSIS/project.2/fastq_to_align_MR
@@ -24,46 +26,7 @@ GFF=/data/cgsb/carlton/MB_ddRAD/MB_BACKUP/TD_SEQ_PAPAER_ANALYSIS/BWA_genome_inde
 
 
 
-# ** Genome Version **
-
-#GENOMEVERSION is used by snpEff.  It should be the basename of your
-#fasta-formatted reference (e.g., basename.fa), and should be 
-#the same as (1) the "genome name" provided in the "genome:" section
-#your v2.0.5 snpEff.config file, and (2) the basename of your 
-#basename.genome entry in the "Genome details" section of your 
-#v2.0.5 snpEff.config     (see also the pipeline README)
-
-#Example: for GENOMEVERSION=Creinhardtii_236_cm, the 
-#reference would be Creinhardtii_236_cm.fa, the "genome:" entry would
-#in snpEff.config be Creinhardtii_236_cm, and the "Genome details"
-# entry would be Creinhardtii_236_cm.genome
-
-
-#I should crate new genome version for SNPEFF, as now Tvag 2.0 genome version is released. 
-
 GENOMEVERSION=TrichDB-2.0_TvaginalisG3_Genome_cleaned
-
-
-# ** snpEff **
-
-#note: set path to snpEff directory (which contains the snpEff.jar) and 
-#snpEff.config these will normally be in your home directory
-
-SNPEFFPATH=/home/mb3188/snpEff
-SNPEFFCONFIG=/home/mb3188/snpEff/snpEff.config
-
-
-# ** snp database for base quality recalibration **
-
-#note: only used in base recalibration step (bqsr_master.sh). May
-#      leave undefined if snpdb is created by an initial round of
-#      of snp-calling using the pipeline (i.e., when there is
-#      not a snpdb available).
-#note: SNPDB will usually be a full path to a vcf file. Please 
-#      check GATK BaseRecalibrator documentation if your snpdb
-#      is in a different format.
-
-SNPDB=
 
 
 # ** callable depths **
@@ -135,44 +98,6 @@ VCFTOOLSMOD=vcftools/intel/0.1.12a
 FILOMOD=filo/intel/1.1.0
 FILOPATH=/share/apps/filo/1.1.0/intel/bin/
 
-#snpeff
-#note: no backward comptability with databases when install new version, so need to remake when upgrade to 3.0
-#note: if upgrade to v3.0, need to change -o vcf to -o gatk in in snpEff_pipeline.sh (see snpEff FAQ)
-#note: if use new genome, $GENOME must match the name in the snpEff data dir
-#note: notice snpEff.config has data_dir var that provides path to snpeff data dir.
-#         may also need to be adjusted when using new genome
-#note: snpEff_pipeline.sh uses -treatAllAsProteinCoding Auto. This will cause
-#         all transcripts to be treated as being protein coding (true for chlamy and datepalm?) 
-#         May need to be adjusted for other genomes.
-
-#Set the genome version for snpEff.  This is the (geneome).fa : something in the snpEff.config file
-#From the snpEff.config file:
-## Databases & Genomes
-#
-# One entry per genome version.
-#
-# For genome version 'ZZZ' the entries look like
-#	ZZZ.genome              : Real name for ZZZ (e.g. 'Human')
-
-#  Example: In the "genome" section of the snpEff.config
-#  genome: \
-#  , GRCh37.65 \  #<- this is an example entry
-
-#  Same example, in the section beginning:
-#  #---
-#  # Genome details
-#  #---
-#  #ENSEMBL v65 : Homo_sapiens.GRCh37.65
-#  GRCh37.65.genome : Homo_sapiens
-
-#The snpEff GENOMEVERSION variable would be GRCh37.65 for the above example.
-
-#note: don't upgrade snpeff unless first verify that GATK will take new version of snpeff output (which it wouldn't as of 2.10.13)
-
-
-
-
-
 #checks for some files that might be commonly mis-specified
 
 if [ ! -f ${REF} ]; then
@@ -191,15 +116,6 @@ if [ ! -f ${FQTABLE} ]; then
         exit 1
 fi
 
-if [ ! -f ${SNPEFFCONFIG} ]; then
-	echo _ERROR_ snpEff.config file [ ${SNPEFFCONFIG} ] not found
-	exit 1
-fi
 
-if [ ! -f ${SNPEFFPATH}/snpEff.jar ]; then
-	echo _ERROR_ snpEff.jar [ ${SNPEFFPATH}/snpEff.jar ] not found
-	exit 1
-fi
-
-#validate fastq table format
-#perl -ne 'die("_ERROR_ error in fastq table format\n") unless(/^\S+\t\S+\t\S+$/)' ${FQTABLE}
+#validate fastq table format, validates that FASTQ table is in a proper format. 
+perl -ne 'die("_ERROR_ error in fastq table format\n") unless(/^\S+\t\S+\t\S+$/)' ${FQTABLE}
